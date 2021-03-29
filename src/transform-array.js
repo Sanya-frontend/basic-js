804454a7dc66b82a79cd2e85a147d5606b4477bc
Sanya-frontend <79463917+Sanya-frontend@users.noreply.github.com>
2021-03-29 21:18:29 +0300

    feat: add function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,5 +1,29 @@
 const CustomError = require("../extensions/custom-error");
-
 module.exports = function transform(arr) {
-
+  if (arr.length == undefined) {
+    return "Error";
+  };
+  let res = [];
+  let a = arr.slice();
+  for (let i = 0; i < a.length; i++){
+    if (a[i] == '--discard-next') {
+       i++;
+       a[i] = undefined;
+      continue;
+      };
+    if (a[i] == '--discard-prev') { 
+      if(a[i - 1] !== undefined) res.pop();
+      continue;
+      };
+    if (a[i] == '--double-next') {
+       res.push(a[i + 1]);
+      continue;
+      };
+    if (a[i] == '--double-prev') {
+       res.push(a[i - 1]);
+      continue;
+      };
+    res.push(a[i]);
+  }
+  return res.filter((item) => item !== undefined);
 };
