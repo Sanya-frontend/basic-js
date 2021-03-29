2771c470392a1f717564daa57180f27ad4b6ce5d
Sanya-frontend <79463917+Sanya-frontend@users.noreply.github.com>
2021-03-29 12:40:25 +0300

    fix: add function

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,5 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-  throw new CustomError('Not implemented');
+  let turns = Math.pow(2, disksNumber) - 1;
+  let seconds = Math.floor((turns * 1*60*60) / turnsSpeed);
+  return { turns, seconds };
 };
