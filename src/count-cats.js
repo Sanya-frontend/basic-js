5a155daba6d85ff67b8e5151c2c5199bde979dc7
Sanya-frontend <79463917+Sanya-frontend@users.noreply.github.com>
2021-03-29 06:10:51 +0300

    feat: add function

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,5 +1,12 @@
-const CustomError = require("../extensions/custom-error");
-
 module.exports = function countCats(matrix) {
-  
-};
+    let count = 0;
+    let number = matrix.length;
+      for (let i = 0; i < number; i++) {
+        matrix[i].forEach(element => {
+          if (element === "^^") {
+            count++;
+          }
+        });
+      }
+    return count;
+  };
