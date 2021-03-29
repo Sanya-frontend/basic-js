2ea7659354af35c28f2f5f897dcd7b5f71bd69eb
Sanya-frontend <79463917+Sanya-frontend@users.noreply.github.com>
2021-03-29 21:46:25 +0300

    feat: add function

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,7 +1,19 @@
 const CustomError = require("../extensions/custom-error");
-
 module.exports = class DepthCalculator {
   calculateDepth(arr) {
+    if (!Array.isArray(arr)) {
+      return 1;
+    };
 
+    let maxDepth = 1;
+    arr.forEach(element => {
+      if (Array.isArray(element)) {
+        const depth = this.calculateDepth(element) + 1;
+        if (maxDepth < depth) {
+          maxDepth = depth;
+        };
+      }
+      });
+    return maxDepth;
   }
 };
