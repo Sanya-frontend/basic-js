ad54e5931685dc4636162208f0431120b48491cb
Sanya-frontend <79463917+Sanya-frontend@users.noreply.github.com>
2021-03-29 12:41:34 +0300

    feat: add function

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -4,4 +4,4 @@ module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
   let turns = Math.pow(2, disksNumber) - 1;
   let seconds = Math.floor((turns * 1*60*60) / turnsSpeed);
   return { turns, seconds };
-};
+}; 
