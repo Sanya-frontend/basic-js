395774148c97954884ec24dbe45bb7dfcacaff56
Sanya-frontend <79463917+Sanya-frontend@users.noreply.github.com>
2021-03-29 20:21:31 +0300

    feat: add function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,6 +1,32 @@
+const { usingPromise } = require("sinon");
 const CustomError = require("../extensions/custom-error");
-
 module.exports = function repeater(str, options) {
+  let result = [];
+  str = String(str);
+  if (options.addition !== undefined) {
+    options.addition = String(options.addition);
+  }
 
+  if (options.additionRepeatTimes === undefined && options.addition !== undefined) {
+    str += options.addition;
+  }
+
+  for (let i = 0; i < options.additionRepeatTimes; i++) {
+    str = str + options.addition;
+    if (i !== options.additionRepeatTimes - 1) {
+      if (options.additionSeparator === undefined) {
+        options.additionSeparator = '|';
+      }
+      str = str + options.additionSeparator;
+    }
+  }
+  result = str;
+
+  for (let i = 1; i < options.repeatTimes; i++) {
+    if (options.separator === undefined) {
+      options.separator = '+';
+    }
+    result += options.separator + str;
+  }
+  return result;
 };
-  
