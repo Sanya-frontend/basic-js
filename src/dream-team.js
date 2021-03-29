0c4c99ef4a9c09a93946764cdb9dce04a617ea9a
Sanya-frontend <79463917+Sanya-frontend@users.noreply.github.com>
2021-03-29 14:06:31 +0300

    feat: add function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,5 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-
+  if(!Array.isArray(members)) {
+    return false;
+   }
+  let dreamTeam = [];
+  let word = members[i];
+  for (let i = 0; i < members.length; i++) {
+    if (typeof word === "string") {
+      dreamTeam.push(word.trim().charAt().toUpperCase());
+    }
+    return dreamTeam.sort().join('');
+  }
 };
