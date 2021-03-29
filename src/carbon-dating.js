b3befae8c7927382dfa54773146fbf35d18c3fb4
Sanya-frontend <79463917+Sanya-frontend@users.noreply.github.com>
2021-03-29 22:45:57 +0300

    feat: add function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,11 @@
 const CustomError = require("../extensions/custom-error");
-
-const MODERN_ACTIVITY= 15; 
+const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  throw new CustomError('Not implemented');
-
+  if ((typeof sampleActivity) !== 'string'|| sampleActivity > 15 || sampleActivity <= 0 || isNaN(sampleActivity)) {
+    return false;
+  };
+  let i = 0.693 / HALF_LIFE_PERIOD;
+  return Math.ceil(Math.log(MODERN_ACTIVITY/sampleActivity) / i);
 };
